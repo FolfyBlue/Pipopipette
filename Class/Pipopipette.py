51c0d356bdde9bc81598de1f08e6308625be5875
Joshua Vandaële <joshua@vandaele.software>
2022-10-18 20:40:19 +0200

    Rewrite set_side

--- Class/Pipopipette.py
+++ Class/Pipopipette.py
@@ -10,9 +10,9 @@ class Pipopipette():
     """
     The class that will manage the playground.
     """
-    
+
     # <----- init ----->
-    
+
     def __init__(self: Pipopipette, width: int = 5, height: int = 5) -> None:
         """
 
@@ -23,20 +23,20 @@ class Pipopipette():
         self.__width: Final[int] = width
         self.__height: Final[int] = height
         self.__list_square: list[Square] = []
-        
+
         for i in range(width * height): self.__list_square.append(Square(i))
-    
+
     # <----- getter ----->
-    
+
     @property
     def width(self: Pipopipette) -> int: return self.__width
-    
+
     @property
     def height(self: Pipopipette) -> int: return self.__height
-    
+
     @property
     def list_square(self: Pipopipette) -> list[Square]: return self.__list_square
-    
+
     # <----- setter ----->
 
     @list_square.setter
@@ -48,9 +48,9 @@ class Pipopipette():
         return_str = ""
         for square in self.__list_square: return_str += square.__str__() + " "
         return return_str
-    
+
      # <----- get_square_by_ID ----->
-    
+
     def get_square_by_ID(self: Pipopipette, id: int) -> Square | None:
         """
         Used to get a Square by his ID
@@ -63,34 +63,38 @@ class Pipopipette():
         for square in self.__list_square:
             if square.id == id: return square
         return None
-    
-    # <----- setSide ----->
-    
-    def set_side(self: Pipopipette, squareID: int, side: str, owner_ID: int) ->  None:
-        """
-        Define the side of a Square.
-        Args:
-            squareID: The ID of the Squre to edit
-            side: 'l'; 'r', 't', or 'd'. Wich side to edit ?
-            ownerID: The player who placed this side
 
+    # <----- setSide ----->
+
+    def set_side(self: Pipopipette, squareID: int, side: str, owner_ID: int) -> None:
+        """Defines the owner of a side of a square.
+
+        Args:
+            squareID (int): ID of the square to edit.
+            side (str): 'l', 'r', 't', or 'd'. Side to edit.
+            owner_ID (int): ID Of the player who now owns this side.
         """
-        if self.get_square_by_ID(squareID) != None: 
-            if side == 'l':
-                self.get_square_by_ID(squareID).left.owner_ID = owner_ID
-                if self.get_square_by_ID(squareID-1) != None: self.get_square_by_ID(squareID-1).right.owner_ID = owner_ID
-            elif side == 'r':
-                self.get_square_by_ID(squareID).right.owner_ID = owner_ID
-                if self.get_square_by_ID(squareID+1) != None: self.get_square_by_ID(squareID+1).left.owner_ID = owner_ID
-            elif side == 't':
-                self.get_square_by_ID(squareID).top.owner_ID = owner_ID
-                if self.get_square_by_ID(squareID-self.__height) != None: self.get_square_by_ID(squareID-self.__height).down.owner_ID = owner_ID
-            elif side == 'd':
-                self.get_square_by_ID(squareID).down.owner_ID = owner_ID
-                if self.get_square_by_ID(squareID+self.__height) != None: self.get_square_by_ID(squareID+self.__height).top.owner_ID = owner_ID
-            
+        if (square := self.get_square_by_ID(squareID)) != None:
+            match side:
+                case 'l':
+                    square.left.owner_ID = owner_ID
+                    if (neighbor := self.get_square_by_ID(squareID-1)) != None:
+                        neighbor.right.owner_ID = owner_ID
+                case 'r':
+                    square.right.owner_ID = owner_ID
+                    if (neighbor := self.get_square_by_ID(squareID+1)) != None:
+                        neighbor.left.owner_ID = owner_ID
+                case 't':
+                    square.top.owner_ID = owner_ID
+                    if (neighbor := self.get_square_by_ID(squareID-self.__height)) != None:
+                        neighbor.down.owner_ID = owner_ID
+                case 'd':
+                    square.down.owner_ID = owner_ID
+                    if (neighbor := self.get_square_by_ID(squareID+self.__height)) != None:
+                        neighbor.top.owner_ID = owner_ID
+
     # <----- valideTarget ----->
-    
+
     def valid_target(self: Pipopipette, squareID: int, side: str) -> bool:
         """
         Can a player place a Segment here ?
@@ -107,4 +111,4 @@ class Pipopipette():
                 (side == 'r' and self.get_square_by_ID(squareID).right.owner_ID != -1) or
                 (side == 't' and self.get_square_by_ID(squareID).top.owner_ID != -1) or
                 (side == 'd' and self.get_square_by_ID(squareID).down.owner_ID != -1)): return True
-        return False 
+        return False
