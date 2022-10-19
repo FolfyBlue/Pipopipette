8fe6033902accfdb896fbd801d0860fe1d63180a
LJ5O <75009579+LJ5O@users.noreply.github.com>
2022-10-19 15:43:19 +0200

    Added loadPlayer function
    Test user :
    username : test,
    password : 1234

--- SaveSystem/SaveSystem.py
+++ SaveSystem/SaveSystem.py
@@ -1,6 +1,7 @@
 class Player():  # Used just to avoid errors on compilation. Delete this when the real Plater class is ready !
     pass
 
+
 import json
 import bcrypt
 
@@ -33,7 +34,20 @@ class SaveSystem():
         Returns: Player if found someone with this username and password, None if something went wrong
 
         """
-        pass
+        with open("../gameData/players.json") as jsonFile:  # Could be an error if used as imported module ?
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+
+        for element in jsonObject:
+            if element['username'] == username:
+                if bcrypt.checkpw(str.encode(password), str.encode(element['password'])):
+                    print("FOUND")
+                    raise Exception("We need to implement a Player class to return a Player here")
+                    # TODO : Add Player class and create a player here
+                else:
+                    # User found, but wrong password. We don't need to continue looking for the right user
+                    return None
+        return None  # Don't found this user
 
     @staticmethod
     def createNewUser(username: str, password: str, id: int, points: int = 0) -> Player:
@@ -48,3 +62,11 @@ class SaveSystem():
         Returns: A Player object
         """
         pass
+
+
+if __name__ == '__main__':
+    # Unit test
+    # print(bcrypt.hashpw(b'1234', bcrypt.gensalt()))
+    # print(bcrypt.checkpw(b'1234', b'$2b$12$Fz6ZG4GIm0jRkN35acYaXeoDMPlPQGcclnChced.W.ivZnI5YWv16'))
+
+    SaveSystem.loadPlayer('test', '1234')
