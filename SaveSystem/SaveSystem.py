f53377df9262431d2b188e42e3a70e381fe56f5c
LJ5O <75009579+LJ5O@users.noreply.github.com>
2022-10-20 10:22:03 +0200

    Added functions in saveSystem
    We will need to implement the real class Player to finish these functions

--- SaveSystem/SaveSystem.py
+++ SaveSystem/SaveSystem.py
@@ -1,9 +1,21 @@
-class Player():  # Used just to avoid errors on compilation. Delete this when the real Plater class is ready !
-    pass
+class Player():  # TODO : Used just to avoid errors on compilation. Delete this when the real Plater class is ready !
+    def getUsername(self):
+        pass
+
+    def getPassword(self):
+        pass
+
+    def getId(self):
+        pass
+
+    def getPoints(self):
+        pass
 
 
 import json
 import bcrypt
+
+SAVE_FILE_PATH = "../gameData/players.json"
 
 
 class SaveSystem():
@@ -21,7 +33,25 @@ class SaveSystem():
         Args:
             player: The Player to save
         """
-        pass
+        with open(SAVE_FILE_PATH) as jsonFile:#File loaded
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+
+        newEntry = {}#I create a new entry that will replace the old one
+        newEntry['username'] = "Replace me" # TODO
+        newEntry['password'] = "1234" # TODO : get password and hash it
+        newEntry['id'] = "987123"
+        newEntry['points'] = 20
+
+        for i in range(len(jsonObject)):#Loop to get the right entry for this user
+            if jsonObject[i]['username'] == player.getUsername():
+                if bcrypt.checkpw(str.encode(player.getPassword()), str.encode(jsonObject[i]['password'])):
+                    # Found user to edit
+                    jsonObject[i] = newEntry#Old entry replaced
+
+        with open(SAVE_FILE_PATH, "w") as file:#The new file with data is written
+            json.dump(jsonObject, file)
+            file.close()
 
     @staticmethod
     def loadPlayer(username: str, password: str) -> Player | None:
@@ -34,7 +64,7 @@ class SaveSystem():
         Returns: Player if found someone with this username and password, None if something went wrong
 
         """
-        with open("../gameData/players.json") as jsonFile:  # Could be an error if used as imported module ?
+        with open(SAVE_FILE_PATH) as jsonFile:
             jsonObject = json.load(jsonFile)
             jsonFile.close()
 
@@ -61,7 +91,22 @@ class SaveSystem():
 
         Returns: A Player object
         """
-        pass
+        with open(SAVE_FILE_PATH) as jsonFile:
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
+
+        newEntry = {'username': username,
+                    'password': bcrypt.hashpw(str.encode(password), bcrypt.gensalt()).decode("utf-8"),
+                    'id': id,
+                    'points': points}
+
+        jsonObject.append(newEntry)
+
+        with open(SAVE_FILE_PATH, "w") as file:
+            json.dump(jsonObject, file)
+            file.close()
+
+        # TODO : add Player return
 
 
 if __name__ == '__main__':
@@ -69,4 +114,6 @@ if __name__ == '__main__':
     # print(bcrypt.hashpw(b'1234', bcrypt.gensalt()))
     # print(bcrypt.checkpw(b'1234', b'$2b$12$Fz6ZG4GIm0jRkN35acYaXeoDMPlPQGcclnChced.W.ivZnI5YWv16'))
 
-    SaveSystem.loadPlayer('test', '1234')
+    #SaveSystem.loadPlayer('test', '1234')
+    #SaveSystem.savePlayer(None)
+    SaveSystem.createNewUser("TEST1234", "9876", 7326576, 8)
