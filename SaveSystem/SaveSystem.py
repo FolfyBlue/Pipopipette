17dafba517fba20a9246c405f8aa7450023cb73a
Joshua Vandaële <joshua@vandaele.software>
2022-10-24 17:17:42 +0200

    Follow pep8 variable/function naming convention

--- SaveSystem/SaveSystem.py
+++ SaveSystem/SaveSystem.py
@@ -1,14 +1,14 @@
 class Player():  # TODO : Used just to avoid errors on compilation. Delete this when the real Plater class is ready !
-    def getUsername(self):
+    def get_username(self):
         pass
 
-    def getPassword(self):
+    def get_password(self):
         pass
 
-    def getId(self):
+    def get_id(self):
         pass
 
-    def getPoints(self):
+    def get_points(self):
         pass
 
 
@@ -27,34 +27,34 @@ class SaveSystem():
         raise Exception("This is a static class !")
 
     @staticmethod
-    def savePlayer(player: Player) -> None:
+    def save_player(player: Player) -> None:
         """
         Save a player object in storage
         Args:
             player: The Player to save
         """
-        with open(SAVE_FILE_PATH) as jsonFile:#File loaded
-            jsonObject = json.load(jsonFile)
-            jsonFile.close()
+        with open(SAVE_FILE_PATH) as json_file:#File loaded
+            json_object = json.load(json_file)
+            json_file.close()
 
-        newEntry = {}#I create a new entry that will replace the old one
-        newEntry['username'] = "Replace me" # TODO
-        newEntry['password'] = "1234" # TODO : get password and hash it
-        newEntry['id'] = "987123"
-        newEntry['points'] = 20
+        new_entry = {}#I create a new entry that will replace the old one
+        new_entry['username'] = "Replace me" # TODO
+        new_entry['password'] = "1234" # TODO : get password and hash it
+        new_entry['id'] = "987123"
+        new_entry['points'] = 20
 
-        for i in range(len(jsonObject)):#Loop to get the right entry for this user
-            if jsonObject[i]['username'] == player.getUsername():
-                if bcrypt.checkpw(str.encode(player.getPassword()), str.encode(jsonObject[i]['password'])):
+        for i in range(len(json_object)):#Loop to get the right entry for this user
+            if json_object[i]['username'] == player.get_username():
+                if bcrypt.checkpw(str.encode(player.get_password()), str.encode(json_object[i]['password'])):
                     # Found user to edit
-                    jsonObject[i] = newEntry#Old entry replaced
+                    json_object[i] = new_entry#Old entry replaced
 
         with open(SAVE_FILE_PATH, "w") as file:#The new file with data is written
-            json.dump(jsonObject, file)
+            json.dump(json_object, file)
             file.close()
 
     @staticmethod
-    def loadPlayer(username: str, password: str) -> Player | None:
+    def load_player(username: str, password: str) -> Player | None:
         """
         Get the saved data of someone, with his username and password
         Args:
@@ -64,11 +64,11 @@ class SaveSystem():
         Returns: Player if found someone with this username and password, None if something went wrong
 
         """
-        with open(SAVE_FILE_PATH) as jsonFile:
-            jsonObject = json.load(jsonFile)
-            jsonFile.close()
+        with open(SAVE_FILE_PATH) as json_file:
+            json_object = json.load(json_file)
+            json_file.close()
 
-        for element in jsonObject:
+        for element in json_object:
             if element['username'] == username:
                 if bcrypt.checkpw(str.encode(password), str.encode(element['password'])):
                     print("FOUND")
@@ -80,7 +80,7 @@ class SaveSystem():
         return None  # Don't found this user
 
     @staticmethod
-    def createNewUser(username: str, password: str, id: int, points: int = 0) -> Player:
+    def create_user(username: str, password: str, id: int, points: int = 0) -> Player:
         """
         Register a new user in the save system
         Args:
@@ -91,19 +91,19 @@ class SaveSystem():
 
         Returns: A Player object
         """
-        with open(SAVE_FILE_PATH) as jsonFile:
-            jsonObject = json.load(jsonFile)
-            jsonFile.close()
+        with open(SAVE_FILE_PATH) as json_file:
+            json_object = json.load(json_file)
+            json_file.close()
 
-        newEntry = {'username': username,
+        new_entry = {'username': username,
                     'password': bcrypt.hashpw(str.encode(password), bcrypt.gensalt()).decode("utf-8"),
                     'id': id,
                     'points': points}
 
-        jsonObject.append(newEntry)
+        json_object.append(new_entry)
 
         with open(SAVE_FILE_PATH, "w") as file:
-            json.dump(jsonObject, file)
+            json.dump(json_object, file)
             file.close()
 
         # TODO : add Player return
@@ -116,4 +116,4 @@ if __name__ == '__main__':
 
     #SaveSystem.loadPlayer('test', '1234')
     #SaveSystem.savePlayer(None)
-    SaveSystem.createNewUser("TEST1234", "9876", 7326576, 8)
+    SaveSystem.create_user("TEST1234", "9876", 7326576, 8)
