118fc5597275273a77abf3bcf7e582d9b2289a15
Joshua Vandaële <joshua@vandaele.software>
2022-10-24 16:57:57 +0200

    Save System ready
    Should be ready to be used

--- SaveSystem/SaveSystem.py
+++ SaveSystem/SaveSystem.py
@@ -1,3 +1,5 @@
+from jeu.engine.Player.Player import Player
+
 import json
 import bcrypt
 
@@ -26,28 +28,28 @@ class SaveSystem():
     @staticmethod
     def save_player(player: Player) -> None:
         """
-        Save a player object in storage
+        Save a player object in storage. Will not update the password for this player
         Args:
             player: The Player to save
         """
-        with open(SAVE_FILE_PATH) as json_file:  # File loaded
-            json_object = json.load(json_file)
-            json_file.close()
+        with open(SAVE_FILE_PATH) as jsonFile:  # File loaded
+            jsonObject = json.load(jsonFile)
+            jsonFile.close()
 
-        new_entry = {}  # I create a new entry that will replace the old one
-        new_entry['username'] = "Replace me"  # TODO
-        new_entry['password'] = "1234"  # TODO : get password and hash it
-        new_entry['id'] = "987123"
-        new_entry['points'] = 20
+        for i in range(len(jsonObject)):  # Loop to get the right entry for this user
+            if jsonObject[i]['username'] == player.NAME:
+                # if bcrypt.checkpw(str.encode(player.getPassword()), str.encode(jsonObject[i]['password'])):
+                # Found user to edit
+                newEntry = {}  # I create a new entry that will replace the old one
+                newEntry['username'] = player.NAME
+                newEntry['password'] = jsonObject[i]['password']
+                newEntry['id'] = player.ID
+                newEntry['points'] = player.score.value
 
-        for i in range(len(json_object)):  # Loop to get the right entry for this user
-            if json_object[i]['username'] == player.get_username():
-                if bcrypt.checkpw(str.encode(player.get_password()), str.encode(json_object[i]['password'])):
-                    # Found user to edit
-                    json_object[i] = new_entry  # Old entry replaced
+                jsonObject[i] = newEntry  # Old entry replaced
 
         with open(SAVE_FILE_PATH, "w") as file:  # The new file with data is written
-            json.dump(json_object, file)
+            json.dump(jsonObject, file)
             file.close()
 
     @staticmethod
@@ -68,9 +70,7 @@ class SaveSystem():
         for element in json_object:
             if element['username'] == username:
                 if bcrypt.checkpw(str.encode(password), str.encode(element['password'])):
-                    print("FOUND")
-                    raise NotImplementedError("We need to implement a Player class to return a Player here")
-                    # TODO : Add Player class and create a player here
+                    return Player(username, element['id'], element['points'])
                 else:
                     # User found, but wrong password. We don't need to continue looking for the right user
                     return None
@@ -105,14 +105,15 @@ class SaveSystem():
             json.dump(json_object, file)
             file.close()
 
-        # TODO : add Player return
+        return Player(username, id, points)
 
 
 if __name__ == '__main__':
     # Unit test
-    # print(bcrypt.hashpw(b'1234', bcrypt.gensalt()))
-    # print(bcrypt.checkpw(b'1234', b'$2b$12$Fz6ZG4GIm0jRkN35acYaXeoDMPlPQGcclnChced.W.ivZnI5YWv16'))
+    print(bcrypt.hashpw(b'1234', bcrypt.gensalt()))
+    print(bcrypt.checkpw(b'1234', b'$2b$12$Fz6ZG4GIm0jRkN35acYaXeoDMPlPQGcclnChced.W.ivZnI5YWv16'))
 
-    #SaveSystem.loadPlayer('test', '1234')
-    # SaveSystem.savePlayer(None)
-    SaveSystem.create_user("TEST1234", "9876", 7326576, 8)
+    player: Player = SaveSystem.loadPlayer('test', '1234')
+    print(player)
+    SaveSystem.savePlayer(player)
+    #SaveSystem.createNewUser("TEST1234", "9876", 7326576, 8)
