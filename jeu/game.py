5617fbad3eea9b7a8136f69d294655203cb2d7d8
Joshua Vandaële <joshua@vandaele.software>
2022-12-06 17:26:02 +0100

    V0.a - Currently not working

--- jeu/game.py
+++ jeu/game.py
@@ -7,21 +7,28 @@ For now, we are just implementing their potential look and functionality.
 import pygame
 from jeu.utils.font_manager import FontManager
 from jeu.utils.assets_import import resource_path
+from jeu.ui.button import Button
+from jeu.ui.ui import UI
 import time
+
 
 def formatted_score(score: int) -> str:
     return f"{score:03d}"
+
 
 def formatted_time(time_in_seconds: int) -> str:
     minutes = time_in_seconds // 60
     seconds = time_in_seconds % 60
     return f"{minutes:02d}:{seconds:02d}"
 
+
 def get_timer_label(start_time_in_seconds: float, font: FontManager):
     time_elapsed_in_seconds = int(time.time() - start_time_in_seconds)
-    timer_label: pygame.surface.Surface = font.get_font(75).render(formatted_time(time_elapsed_in_seconds), True, "#EEEEEE")
+    timer_label: pygame.surface.Surface = font.get_font(75).render(
+        formatted_time(time_elapsed_in_seconds), True, "#EEEEEE")
     timer_rect: pygame.rect.Rect = timer_label.get_rect(center=(640, 50))
     return (timer_label, timer_rect)
+
 
 def get_score_label(score: int, font: FontManager, player1: bool):
     if player1:
@@ -30,9 +37,12 @@ def get_score_label(score: int, font: FontManager, player1: bool):
     else:
         xpos: int = 1280-100
         color: str = "#FF0000"
-    player_score_label: pygame.surface.Surface = font.get_font(75).render(f"{score:03d}", True, color)
-    player_score_rect: pygame.rect.Rect = player_score_label.get_rect(center=(xpos, 650))
+    player_score_label: pygame.surface.Surface = font.get_font(
+        75).render(f"{score:03d}", True, color)
+    player_score_rect: pygame.rect.Rect = player_score_label.get_rect(
+        center=(xpos, 650))
     return (player_score_label, player_score_rect)
+
 
 def game(screen: pygame.surface.Surface, size: tuple[int, int] = (10, 10)):
     """Game screen
@@ -43,19 +53,24 @@ def game(screen: pygame.surface.Surface, size: tuple[int, int] = (10, 10)):
     clock: pygame.time.Clock = pygame.time.Clock()
     pygame.display.set_caption("Pipopipette")
 
-    game_font: FontManager = FontManager(resource_path("jeu/assets/fonts/Truculenta.ttf"))
+    game_font: FontManager = FontManager(
+        resource_path("jeu/assets/fonts/Truculenta.ttf"))
 
-    background: pygame.surface.Surface = pygame.image.load(resource_path("jeu/assets/images/game_background.png"))
+    background: pygame.surface.Surface = pygame.image.load(
+        resource_path("jeu/assets/images/game_background.png"))
 
     labels: dict[str, tuple[pygame.surface.Surface, pygame.rect.Rect]] = {}
     start_time_in_seconds: float = time.time()
 
-    player1_label: pygame.surface.Surface = game_font.get_font(33).render("Playername01", True, "#EEEEEE")
+    player1_label: pygame.surface.Surface = game_font.get_font(
+        33).render("Playername01", True, "#EEEEEE")
     player1_rect: pygame.rect.Rect = player1_label.get_rect(center=(100, 593))
-    player2_label: pygame.surface.Surface = game_font.get_font(33).render("Playername02", True, "#EEEEEE")
-    player2_rect: pygame.rect.Rect = player2_label.get_rect(center=(1280-100, 593))
+    player2_label: pygame.surface.Surface = game_font.get_font(
+        33).render("Playername02", True, "#EEEEEE")
+    player2_rect: pygame.rect.Rect = player2_label.get_rect(
+        center=(1280-100, 593))
 
-    labels["timer"] =  get_timer_label(time.time(), game_font)
+    labels["timer"] = get_timer_label(time.time(), game_font)
     labels["player1"] = (player1_label, player1_rect)
     labels["player2"] = (player2_label, player2_rect)
     labels["player1_score"] = get_score_label(0, game_font, True)
@@ -63,6 +78,33 @@ def game(screen: pygame.surface.Surface, size: tuple[int, int] = (10, 10)):
 
     started = False
 
+    grid_height = 1280-200
+    grid_width = 720-200
+    segments_heigh_count = grid_height//size[0]
+    segments_width_count = grid_width//size[1]
+
+    board_elements: list[UI] = []
+
+    def draw_board():
+        board_elements = []
+        cur_height = 0
+        for i in range(segments_width_count):
+            segment: Button = Button(
+                screen=screen,
+                image=pygame.image.load(resource_path(
+                    "jeu/assets/images/square.png")),
+                position=(250, i*120),
+                text="",
+                font=game_font.get_font(75),
+                color="BLACK",
+                hover_color="BLACK",
+                action=lambda: print("click"),
+                enforced_size=(100, 50)
+            )
+            print(segment)
+            board_elements.append(segment)
+
+    draw_board()
     while True:
         if started:
             labels["timer"] = get_timer_label(start_time_in_seconds, game_font)
@@ -74,6 +116,8 @@ def game(screen: pygame.surface.Surface, size: tuple[int, int] = (10, 10)):
             screen.blit(surface, rect)
 
         for event in pygame.event.get():
+            for element in board_elements:
+                element.update(event)
             match (event.type):
                 case pygame.QUIT:
                     quit()
@@ -83,6 +127,7 @@ def game(screen: pygame.surface.Surface, size: tuple[int, int] = (10, 10)):
                         start_time_in_seconds = time.time()
                     # Clear FPS counter from console
                     print("            ", end="\r")
-
+        for element in board_elements:
+            element.update_render()
         pygame.display.update()
         clock.tick()
