24c3150342bda994ee3130e3bd1e01c410654526
Joshua Vandaële <joshua@vandaele.software>
2023-01-16 09:51:05 +0100

    Default characters for the textbox + Size Selector

--- jeu/main_menu.py
+++ jeu/main_menu.py
@@ -11,6 +11,8 @@ from jeu.ui.ui import UI
 from jeu.utils.font_manager import FontManager
 from jeu.utils.assets_import import resource_path
 
+MAX_SIZE = 25
+MIN_SIZE = 2
 
 def main_menu(screen: pygame.surface.Surface):
     """Main menu of the game.
@@ -82,10 +84,10 @@ def main_menu(screen: pygame.surface.Surface):
 
         size_popup_custom_size_x_textbox = Textbox(
             screen=size_popup.surface,
-            position=(size_popup.surface.get_size()[0]//2, size_popup.surface.get_size()[1]//2),
-            placeholder_text="a",
+            position=(size_popup.surface.get_size()[0]//2*0.55, size_popup.surface.get_size()[1]//2),
+            placeholder_text=F"{MIN_SIZE}-{MAX_SIZE}",
             placeholder_color="#424242",
-            size=(75,75),
+            size=(125, 75),
             font=menu_font.get_font(56),
             text_color="black",
             background_color="white",
@@ -93,10 +95,69 @@ def main_menu(screen: pygame.surface.Surface):
             accepted_chars = ["0","1","2","3","4","5","6","7","8","9"]
         )
 
+        size_popup_custom_size_y_textbox = Textbox(
+            screen=size_popup.surface,
+            position=(size_popup.surface.get_size()[0]//2*1.15, size_popup.surface.get_size()[1]//2),
+            placeholder_text=F"{MIN_SIZE}-{MAX_SIZE}",
+            placeholder_color="#424242",
+            size=(125, 75),
+            font=menu_font.get_font(56),
+            text_color="black",
+            background_color="white",
+            max_char = 2,
+            accepted_chars = ["0","1","2","3","4","5","6","7","8","9"]
+        )
+
+        def confirm_custom_size_button_handler():
+            x_size: int = 0
+            y_size: int = 0
+            x_size_text: str = size_popup_custom_size_x_textbox.text
+            y_size_text: str = size_popup_custom_size_y_textbox.text
+            if x_size_text:
+                x_size = int(x_size_text)
+            else:
+                x_size = 5
+            
+            if y_size_text:
+                y_size = int(x_size_text)
+            else:
+                y_size = 5
+            
+            if x_size > MAX_SIZE:
+                x_size = MAX_SIZE
+            elif x_size < MIN_SIZE:
+                x_size = MIN_SIZE
+            
+            if y_size > MAX_SIZE:
+                y_size = MAX_SIZE
+            elif y_size < MIN_SIZE:
+                y_size = MIN_SIZE
+            
+            size_popup.close()
+            game(screen, size=(x_size, y_size))
+
+        size_popup_confirm_custom_size_button = Button(
+            screen=size_popup.surface,
+            image=None,
+            position=(size_popup.surface.get_size()[0]//2*1.5, size_popup.surface.get_size()[1]//2),
+            text="OK",
+            font=menu_font.get_font(64),
+            color="white",
+            hover_color="black",
+            action=confirm_custom_size_button_handler
+        )
+
+        
+        x_label: pygame.surface.Surface = menu_font.get_font(96).render(f"x", True, "white")
+        x_rect: pygame.rect.Rect = x_label.get_rect(center=(size_popup.surface.get_size()[0]//2*0.85, size_popup.surface.get_size()[1]//2*0.95))
+
         size_popup.add_ui_element(size_popup_3x3_button)
         size_popup.add_ui_element(size_popup_5x5_button)
         size_popup.add_ui_element(size_popup_7x7_button)
         size_popup.add_ui_element(size_popup_custom_size_x_textbox)
+        size_popup.add_ui_element(size_popup_custom_size_y_textbox)
+        size_popup.add_ui_element(size_popup_confirm_custom_size_button)
+        size_popup.add_rect(x_label, x_rect)
 
         size_popup.run()
 
